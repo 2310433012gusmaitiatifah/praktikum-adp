ec6b3daa898d90b1409cf713b37431e2acec849f
gusmaiti atifah <162479933+2310433012gusmaitiatifah@users.noreply.github.com>
2024-05-28 01:27:58 +0700

    Add files via upload

---
+++ tugas 9.2 praktikum adp gusmaitiatifah-2310433012.py
@@ -0,0 +1,98 @@
+import os
+os.system('cls')
+
+from termcolor import cprint, colored
+
+nama   = ' Gusmaiti Atifah Putri'
+nim    = 2310433012
+shift  = ' 1'
+print('Nama         : '+ nama)
+print('NIM          : ', nim)
+print('Shift        : '+ shift)
+print()
+
+cprint("Program Gambar Rumah\n", "light_green")
+
+print(" "*33, end=" ")
+cprint("_"*50,"red")
+print(" "*27, end=" ")
+cprint("     /                                                  \ ","red")
+print(" "*27, end=" ")
+cprint("    /                                                    \ ","red")
+print(" "*27, end=" ")
+cprint("   /                                                      \ ","red")
+print(" "*27, end=" ")
+cprint("  /                                                        \ ","red")
+print(" "*27, end=" ")
+cprint(" /                                                          \ ","red")
+print(" "*27, end=" ")
+cprint("="*62,"light_blue")
+
+for i in range (3):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*59, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for i in range (1):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*40, end=" ")
+    cprint("__"*4, "grey", end="")
+    print(" "*10, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for j in range (1):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*10, end=" ")
+    cprint("__"*4, "grey", end="")
+    print(" "*20, end=" ")
+    cprint("|        |", "grey", end="")
+    print(" "*9, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for k in range (1):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*9, end=" ")
+    cprint("|__|__|__|", "grey", end="")
+    print(" "*19, end=" ")
+    cprint("|        |", "grey", end="")
+    print(" "*9, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for l in range (1):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*9, end=" ")
+    cprint("|__|__|__|", "grey", end="")
+    print(" "*19, end=" ")
+    cprint("|      x |", "grey", end="")
+    print(" "*9, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for m in range (2):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*39, end=" ")
+    cprint("|        |", "grey", end="")
+    print(" "*9, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for m in range (1):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*39, end=" ")
+    cprint("|________|", "grey", end="")
+    print(" "*9, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+for i in range (2):
+    print(" "*27, end=" ")
+    cprint("|", "light_blue", end="")
+    print(" "*59, end=" ")
+    cprint("|", "light_blue", end="")
+    print()
+print(" "*27, end=" ")
+cprint("="*62,"light_blue")
